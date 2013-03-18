ec704f4be6d72265f7e8dcc6bd34cd7edba8700e
andresfgarcia150 <andrespipe150@gmail.com>
2013-03-17 22:44:53 -0500

    Transformada lista

--- tarea5.py
+++ tarea5.py
@@ -1,12 +1,11 @@
-# **********************************************
-# ********** Universidad de los Andes **********
-# **********   Fisica computacional   **********
-# **********         Tarea 5          **********
-# **********************************************
-# Autores:
-#	Andres Felipe Garcia Albarracin
-#	Andrea Rozo Mendez
-# ********************************************** 
+# ******************************************************************
+# ******************** Universidad de los Andes ********************
+# ********************   Fisica computacional   ********************
+# ********************         Tarea 5          ********************
+# ******************************************************************
+# ***************   Andres Felipe Garcia Albarracin  ***************
+# ***************         Andrea Rozo Mendez	     ***************
+# ****************************************************************** 
 
 # Librerias
 import os, sys
@@ -25,32 +24,32 @@ meses = []
 for i in range(fil):
 	meses = np.append(meses,[i])
 
+anos = meses/12.0 + c[0,0] 
 # Vector del numero de manchas
-manchas = (c[:,2])
+manchas = (c[:,3])
 
 # Grafica numero de manchas en funcion del tiempo
-pylab.scatter(meses,manchas)
-pylab.xlabel('Numero de mes')
-pylab.ylabel('N(m)')
-pylab.title('Numero de manchas solares en funcion del mes')
-pylab.savefig('GraficaManchas')
+pylab.scatter(anos,manchas)
+pylab.xlabel('Ano')
+pylab.ylabel('N(n)')
+pylab.title('Numero de manchas solares en funcion del anho')
 pylab.grid(True)
+pylab.savefig('GraficaManchas')
+
 
 # Transformada de Fourier
 n = fil
-dt = 1 					# 1 mes de intervalo
-fft_manchas = fft(manchas)/n 		# Transformada de Fourier normalizada
-freq = fftfreq(n,dt)			# Vector de frecuencias
+dt = 1 							# 1 mes de intervalo
+fft_manchas = np.fft.fft(manchas)/n 			# Transformada de Fourier normalizada
+freq = np.fft.fftfreq(n,dt)				# Vector de frecuencias
 
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-ax2.bar(freq,abs(fft_manchas))
-ax2.set_xlabel("$Frecuencia [1/mes]$",fontsize=20)
-ax2.set_ylabel("$Manchas*mes$",fontsize=20)
-ax2.set_title("$Transformada\ de\ Fourier\ del\ num\ de\ manchas$", fontsize=20)
-filename = 'GraficaTransformada'
-fig2.savefig(filename + '.jpeg',format = 'jpeg', transparent=True)
-
+fig1 = plt.figure()
+pylab.plot(freq,abs(fft_manchas))
+pylab.xlabel('$f\ [1/mes]$')
+pylab.ylabel('$F\{N\}(f)\ [manchas.mes]$')
+pylab.title('$\mathrm{Espectro\ de\ frecuencias\ del\ numero\ de\ manchas}$', fontsize=20)
+pylab.grid(True)
+pylab.savefig('TransfManchas')
 
 
-print meses
+
